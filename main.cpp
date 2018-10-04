71ca0b16070efecb6e1b322c725ab0de8d430798
gabrielpallotta <gabriel.cardenete@hotmail.com>
2018-10-04 11:37:48 -0300

    Testing on main

--- main.cpp
+++ main.cpp
@@ -1,9 +1,17 @@
 #include <iostream>
+#include "SparseMatrix.hpp"
 
 using namespace std;
 
 int main()
 {
-    cout << "Hello world!" << endl;
-    return 0;
+  SparseMatrix<int>* matrix = new SparseMatrix<int>(0);
+  matrix->put(0, 0, 53);
+  matrix->put(0, 1, 54);
+  cout << matrix->get(0, 0) << endl;
+  cout << matrix->get(0, 1) << endl;
+  matrix->put(0, 1, 57);
+  cout << matrix->get(0, 1) << endl;
+
+  return 0;
 }
